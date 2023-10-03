df13eca6f47f93500f9f4b8f61a7443da43878b2
NATHANrsss <146085373+NATHANrsss@users.noreply.github.com>
2023-10-03 08:48:36 -0300

    macacoo

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+function setup() {
+
+    createCanvas(300, 300);
+    
+    background(100);
+    
+    }
+    
+    function draw() {
+    
+    textAlign(CENTER, CENTER);
+    
+    textSize(50);
+    
+    textFont();
+    
+    frase = "macaco";
+    
+    text(frase, width / 2, height / 2);
+    
+    }
+    
